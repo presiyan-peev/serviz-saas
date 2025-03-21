13596970d6913746a82f689b43d7ef3ddd2a5514
presiyan-peev <pressijan@gmail.com>
2025-03-21 23:29:22 +0200

    add useApi with err handling

--- web/src/composables/useApi.ts
+++ web/src/composables/useApi.ts
@@ -1,1 +1,72 @@
-export function useApi() {}
+import { useErrorHandler } from "./utils/useErrorHandler";
+import axios from "axios";
+
+const Axios = axios.create({
+  baseURL: "https://some-domain.com/api/",
+  timeout: 1000,
+  headers: { "X-Custom-Header": "foobar" },
+});
+
+export function useApi() {
+  const { logApiRequestError, logApiResponseError } = useErrorHandler();
+
+  const handleApiError = (error, url: string) => {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      logApiResponseError(error.response, url);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      logApiResponseError(error.request, url);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error.config);
+  };
+
+  const getApi = async (url: string, params: URLSearchParams) => {
+    try {
+      const response = await Axios.get(url, { params });
+      return response;
+    } catch (error) {
+      handleApiError(error, url);
+    }
+  };
+
+  const postApi = async (url: string, body, params) => {
+    try {
+      const response = await Axios.post(url, body, params);
+      return response;
+    } catch (error) {
+      handleApiError(error, url);
+    }
+  };
+
+  const patchApi = async (url: string, body, params) => {
+    try {
+      const response = await Axios.patch(url, body, params);
+      return response;
+    } catch (error) {
+      handleApiError(error, url);
+    }
+  };
+
+  const deleteApi = async (url: string, body) => {
+    try {
+      const response = await Axios.delete(url, body);
+      return response;
+    } catch (error) {
+      handleApiError(error, url);
+    }
+  };
+
+  return {
+    getApi,
+    postApi,
+    patchApi,
+    deleteApi,
+  };
+}
--- web/src/composables/utils/useErrorHandler.ts
+++ web/src/composables/utils/useErrorHandler.ts
@@ -3,24 +3,39 @@ import { useNotify } from "../useSnackbar";
 const { notifyError } = useNotify();
 
 export function useErrorHandler() {
-  function logErrorToConsole(err, vm, info) {
+  const logErrorToConsole = (err, vm, info) => {
     console.error("Error:", err);
     console.error("Vue component:", vm);
     console.error("Additional info:", info);
-  }
+  };
 
-  function logErrorToConsoleAndNotifyUser(
+  const logErrorToConsoleAndNotifyUser = (
     err,
     vm,
     info,
     errorMessage = "Unhandled Error"
-  ) {
+  ) => {
     logErrorToConsole(err, vm, info);
     notifyError({ title: errorMessage });
-  }
+  };
+
+  const logApiResponseError = (response, url: string) => {
+    console.error("FAILED RESPONSE FROM: ", url);
+    console.error(response.data);
+    console.error(response.status);
+    console.error(response.headers);
+  };
+
+  const logApiRequestError = (request, url: string) => {
+    console.error("FAILED REQUEST TO: ", url);
+    console.error(request.data);
+  };
 
   return {
     logErrorToConsole,
     logErrorToConsoleAndNotifyUser,
+
+    logApiResponseError,
+    logApiRequestError,
   };
 }
